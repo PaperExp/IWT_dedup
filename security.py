182b1c599579d6035529d7843ddb7e5ff2f341f4
TheBestIDE <785515163@qq.com>
2023-09-07 10:55:29 +0800

    use sha256 to compute base

--- security.py
+++ security.py
@@ -2,12 +2,18 @@ from scheme import iwt_dedup
 from iwt import image_to_iwt, iwt_to_image
 from phash import phash
 
+import hashlib
+
 ds_path = './cmp_img/'
 out_path = './result/secu/'
 
 def get_phash(img_path : str):
     approximation, (horizontal, vertical, diagonal) = image_to_iwt(img_path)
     return phash(approximation)
+
+def get_hash(img_path : str):
+    approximation, (horizontal, vertical, diagonal) = image_to_iwt(img_path)
+    return hashlib.sha256(approximation).digest()
 
 def write_phash(phash : int, out_f : str):
     print(phash)
@@ -18,12 +24,17 @@ def write_phash(phash : int, out_f : str):
             phash >>= 8
             cnt += 1
 
-def test_security():
-    ph = get_phash(ds_path + 'medical-record-org1.jpeg')
-    write_phash(ph, 'img1_base.csv')
+def write_hash(hash : bytes, out_f : str):
+    with open(out_path + out_f, 'w') as f:
+        for byte in hash:
+            f.write('%d,\n' % byte)
 
-    ph = get_phash(ds_path + 'medical-record-org2.jpeg')
-    write_phash(ph, 'img2_base.csv')
+def test_security():
+    ph = get_hash(ds_path + 'medical-record-org1.jpeg')
+    write_hash(ph, 'img1_base.csv')
+
+    ph = get_hash(ds_path + 'medical-record-org2.jpeg')
+    write_hash(ph, 'img2_base.csv')
 
 if __name__ == '__main__':
     test_security()
